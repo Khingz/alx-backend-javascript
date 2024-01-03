ae1d647658909666a7b2df3a67579b684df6695a
Khingz <kingsley995mr@gmail.com>
2024-01-03 15:35:05 +0000

    task 2 redefined

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,19 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    const resObj = {
-      status: 200,
-      body: 'Success',
-    };
-    if (promise) {
-      resolve(resObj);
-    } else {
-      const errorObject = new Error();
-      reject(errorObject);
-    }
-  })
-    .then((res) => {
-      console.log('Got a response from the API');
-      // eslint-disable-next-line no-unused-vars
-      const unusedVar = res;
-    });
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
