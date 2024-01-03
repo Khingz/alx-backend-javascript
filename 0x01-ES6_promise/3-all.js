b544c5f5e3bd119d466f20431f81ef8cc325e793
Khingz <kingsley995mr@gmail.com>
2024-01-03 16:20:27 +0000

    taask 5

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import { createUser, uploadPhoto } from './utils';
+
+export default function handleProfileSignup() {
+  const allPromises = Promise.all([createUser(), uploadPhoto()]);
+  return allPromises
+    .then((res) => {
+      console.log(`${res[1].body} ${res[0].firstName} ${res[0].lastName}`);
+    })
+    .catch(() => {
+      console.error('Signup system offline');
+    });
+}
