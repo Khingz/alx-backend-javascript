84d1c63657352fccf7641910bd99e8f3a34c5e83
Khingz <kingsley995mr@gmail.com>
2024-01-04 11:17:55 +0000

    task 1

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const classRoom_1 = new ClassRoom(19);
+  const classRoom_2 = new ClassRoom(20);
+  const classRoom_3 = new ClassRoom(34);
+  return [classRoom_1, classRoom_2, classRoom_3];
+}
