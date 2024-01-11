6c2f3ac474449f76c6ea133e5960671bc4fb765e
Khingz <kingsley995mr@gmail.com>
2024-01-11 18:05:41 +0000

    fix

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -3,7 +3,7 @@ export default function cleanSet(set, startString) {
     return '';
   }
   const tempArr = [];
-  for (const value of set) {
+  for (const value of set.values()) {
     if (value.startsWith(startString)) {
       const valStr = value.substring(startString.length);
       if (valStr && valStr !== value) {
