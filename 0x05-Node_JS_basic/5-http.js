125970dc8e662e16b7ab4942581a5d172309bb1c
khingz <kingsley995mr@gmail.com>
2024-02-27 14:03:18 +0000

    semi satandard

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const db = process.argv[2]
+const db = process.argv[2];
 
 const hostname = '127.0.0.1';
 const port = 1245;
