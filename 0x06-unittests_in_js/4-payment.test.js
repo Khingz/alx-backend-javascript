68cfa6b6c81b324aaf3dbd5960893813deac966e
Kingsley Akpan <54294050+Khingz@users.noreply.github.com>
2024-02-29 18:23:04 +0000

    check failing

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -4,14 +4,14 @@ const sendPaymentRequestToApi = require('./4-payment');
 const Utils = require('./utils');
 
 describe('Stubs', function () {
-  it('', () => {
+  it('Math is the same', () => {
     const stubsUtils = sinon.stubs(Utils, 'calculateNumber');
     stubsUtils.returns(10)
-    const stubsConsole = sinon.stub(console, 'log');
+    const stubsConsole = sinon.stubs(console, 'log');
 
     sendPaymentRequestToApi(100, 20);
 
-    expect(stubsUtils.calledOnceWithExactly('SUM', 100, 20)).to.be.equal(10);
+    expect(stubsUtils.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
     expect(stubsConsole.calledOnceWithExactly('The total is: 10')).to.be.true;
 
     stubsUtils.restore();
